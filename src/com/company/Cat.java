8ebc07f8f949b265e4e5f23776243329a75b355f
n.khaimuldin <n.khaimuldin@astanait.edu.kz>
2022-02-10 16:26:29 +0600

    Implemented class Cat which extends from Animal

---
+++ src/com/company/Cat.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Cat extends Animal{
+
+    public Cat(String name) {
+        super(name);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("Meow");
+    }
+
+    @Override
+    public String toString() {
+        return "Cat name " + getName();
+    }
+}
